7ca26f49a5e5917b9f9107844664d6abf312230d
David Scoffield <davidscoffield01@gmail.com>
2022-08-03 20:14:45 -0300

    rename mongo init scripts

---
+++ resources/mongo/2_create-indexes.js
@@ -0,0 +1,17 @@
+// use database
+print('------------ |X| Creating location field |X| ------------')
+db.accident.updateMany({}, [
+  {
+    $addFields: {
+      location: {
+        type: 'Point',
+        coordinates: ['$Start_Lng', '$Start_Lat'],
+      },
+    },
+  },
+])
+print('------------ |X| FINISH creation location field |X| ------------')
+
+print('------------ |X| Creating 2dsphere index over location field |X| ------------')
+db.accident.createIndex({ location: '2dsphere' })
+print('------------ |X| FINISH creation 2dsphere index over location field |X| ------------')
