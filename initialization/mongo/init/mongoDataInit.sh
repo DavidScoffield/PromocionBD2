973595e7a2928b6e3b1450f600aa942b3182bcef
David Scoffield <davidscoffield01@gmail.com>
2022-08-04 15:14:43 -0300

    Add name for csv in .env and use this in init script in mongo

--- initialization/mongo/init/mongoDataInit.sh
+++ initialization/mongo/init/mongoDataInit.sh
@@ -1,11 +1,16 @@
 #!/bin/bash
 
 # tail -n +2 $1 | mongoimport --host localhost --db $2 --collection $3 --type csv --headerline --fieldTypeString --fieldsType < $1
-# tail -n +2 /resources/tmp/US_Accidents_Dec19.csv > /resources/tmp/US_Accidents_Dec19-without_header.csv
+
+echo "------------ |X| START Formating CSV |X| ------------"
+
+tail -n +2 /resources/csv/$NAME_CSV_FILE.csv > /tmp/csv.csv
+
+echo "------------ |X| FINISH Formating CSV |X| ------------"
 
 echo "------------ |X| Importing data into MongoDB |X| ------------"
 
-mongoimport --username=$MONGO_INITDB_ROOT_USERNAME --password=$MONGO_INITDB_ROOT_PASSWORD --port=27017 --authenticationDatabase "admin" --db $MONGO_INITDB_DATABASE --collection accident --type=csv --drop --parseGrace=autoCast --columnsHaveTypes --fieldFile=/resources/init/fieldsNamesCsv.txt --file=/resources/csv/US_Accidents_Dec19-copia.csv
+mongoimport --username=$MONGO_INITDB_ROOT_USERNAME --password=$MONGO_INITDB_ROOT_PASSWORD --port=27017 --authenticationDatabase "admin" --db $MONGO_INITDB_DATABASE --collection accident --type=csv --drop --parseGrace=autoCast --columnsHaveTypes --fieldFile=/resources/init/fieldsNamesCsv.txt --file=/tmp/csv.csv
 
 echo "------------ |X| Successfully importation |X| ------------"
 
