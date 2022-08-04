f2c4bd0d83a0f89035a7dc75325a9b552975a7ca
David Scoffield <davidscoffield01@gmail.com>
2022-08-04 16:26:05 -0300

    Update info message

--- initialization/postgres/init/postgresDataInit.sh
+++ initialization/postgres/init/postgresDataInit.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 # psql -d database --user=root -c "\COPY accidents_test FROM '/resources/tmp/US_Accidents_Dec19_short-10.csv' DELIMITER ',' CSV HEADER"
-echo "------------ |X| Importing data into MongoDB |X| ------------"
+echo "------------ |X| Importing data into POSTGRES |X| ------------"
 
 psql -d $POSTGRES_DB --user=$POSTGRES_USER --set=csvpath="/resources/csv/$NAME_CSV_FILE.csv" -f /resources/init/commands.sql
 
