537b3a30b7b0e0d443facc56b239fbccb4fde1d7
David Scoffield <davidscoffield01@gmail.com>
2022-08-03 21:27:32 -0300

    Add report message to postgres init file

--- initialization/postgres/init/postgresDataInit.sh
+++ initialization/postgres/init/postgresDataInit.sh
@@ -1,5 +1,8 @@
 #!/bin/bash
 
 # psql -d database --user=root -c "\COPY accidents_test FROM '/resources/tmp/US_Accidents_Dec19_short-10.csv' DELIMITER ',' CSV HEADER"
-echo ""
+echo "------------ |X| Importing data into MongoDB |X| ------------"
+
 psql -d database --user=root -f /resources/postgres/commands.sql
+
+echo "------------ |X| SUCCESSFULLY INITIALIZACION POSTGRES DATABASE |X| ------------"
