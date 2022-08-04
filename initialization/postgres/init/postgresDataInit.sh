3c3acf7721d6db2de4c56d1047457cf9585dc078
David Scoffield <davidscoffield01@gmail.com>
2022-08-04 15:51:07 -0300

    Use environment variables in postgres init script

--- initialization/postgres/init/postgresDataInit.sh
+++ initialization/postgres/init/postgresDataInit.sh
@@ -3,6 +3,6 @@
 # psql -d database --user=root -c "\COPY accidents_test FROM '/resources/tmp/US_Accidents_Dec19_short-10.csv' DELIMITER ',' CSV HEADER"
 echo "------------ |X| Importing data into MongoDB |X| ------------"
 
-psql -d database --user=root -f /resources/init/commands.sql
+psql -d $POSTGRES_DB --user=$POSTGRES_USER --set=csvpath="/resources/csv/$NAME_CSV_FILE.csv" -f /resources/init/commands.sql
 
 echo "------------ |X| SUCCESSFULLY INITIALIZACION POSTGRES DATABASE |X| ------------"
--- initialization/postgres/resources/commands.sql
+++ initialization/postgres/resources/commands.sql
@@ -54,4 +54,4 @@ CREATE TABLE accident (
 );
 -- IMPORT DATA FROM CSV
 COPY accident
-FROM '/resources/csv/US_Accidents_Dec19_short-199.csv' DELIMITER ',' CSV HEADER;
+FROM :'csvpath' DELIMITER ',' CSV HEADER;
