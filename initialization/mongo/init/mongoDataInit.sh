541dbede58417c75da2732a04854d7b98d980caa
suelgarayfranco <92928038+suelgarayfranco@users.noreply.github.com>
2022-08-05 12:53:39 -0300

    changed echoed comments

--- initialization/mongo/init/mongoDataInit.sh
+++ initialization/mongo/init/mongoDataInit.sh
@@ -2,18 +2,18 @@
 
 # tail -n +2 $1 | mongoimport --host localhost --db $2 --collection $3 --type csv --headerline --fieldTypeString --fieldsType < $1
 
-echo "------------ |X| START Formating CSV |X| ------------"
+echo "------------ |X| STARTED Formating CSV |X| ------------"
 
 tail -n +2 /resources/csv/$NAME_CSV_FILE.csv >/tmp/csv.csv
 
-echo "------------ |X| FINISH Formating CSV |X| ------------"
+echo "------------ |X| FINISHED Formating CSV |X| ------------"
 
 echo "------------ |X| Importing data into MongoDB |X| ------------"
 
 mongoimport --username=$MONGO_INITDB_ROOT_USERNAME --password=$MONGO_INITDB_ROOT_PASSWORD --port=27017 --authenticationDatabase "admin" --db $MONGO_INITDB_DATABASE --collection accident --type=csv --drop --parseGrace=autoCast --columnsHaveTypes --fieldFile=/resources/init/fieldsNamesCsv.txt --file=/tmp/csv.csv
 
-echo "------------ |X| Successfully importation |X| ------------"
+echo "------------ |X| Successful import |X| ------------"
 
 mongo --quiet --username=$MONGO_INITDB_ROOT_USERNAME --password=$MONGO_INITDB_ROOT_PASSWORD </resources/init/createIndexes.js
 
-echo "------------ |X| SUCCESSFULLY INITIALIZACION MONGO DATABASE |X| ------------"
+echo "------------ |X| SUCCESSFUL INITIALIZATION MONGO DATABASE |X| ------------"
